18c54fdee2c9fe761d262f7da1fd5ebe11a38016
A-Stroem <asjnyborg@hotmail.com>
2022-09-07 11:34:55 +0200

    test escape key

---
+++ js/incdecfont.js
@@ -0,0 +1,38 @@
+
+out('I am in increase/decrease font')
+
+
+const  pbIncrease = document.querySelector('.pbIncrease')
+const  pbDecrease = document.querySelector('.pbDecrease')
+
+const lineFirst = document.querySelector('.first')
+
+out(lineFirst)
+out('fontsize = ' + lineFirst.style.fontSize)
+lineFirst.style.fontSize = '30px'
+out('fontsize = ' + lineFirst.style.fontSize)
+
+let fontsize = 10
+
+function increaseFontSize(btn) {
+  out(btn)
+  fontsize++
+  out('Fontsize = ' + lineFirst.style.fontSize)
+  lineFirst.style.fontSize = fontsize + 'px'
+}
+
+// pbIncrease.addEventListener('click'), () => increaseFontSize()
+// pbIncrease.addEventListener('click'), (btn) => increaseFontSize(btn)
+
+function testKeyPressed(event) {
+  out(event)
+  out(event.key)
+  let k = event.key
+  if (k == 'Escape') {
+    out('dummy')
+  }
+}
+
+pbIncrease.addEventListener('click', increaseFontSize)
+
+document.addEventListener('keydown', e => testKeyPressed(e))
