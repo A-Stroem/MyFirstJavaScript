f650e4311935a5195279454dec3613b599d35765
A-Stroem <asjnyborg@hotmail.com>
2022-09-08 08:50:31 +0200

    updated repo

--- js/setbackgroundcolor.js
+++ js/setbackgroundcolor.js
@@ -1,26 +1,43 @@
 function out(any) {console.log(any)}
 out('I am in setbackgroundcolor')
 
-const inpColor = document.querySelector('.getColor')
-const pbSetColor = document.querySelector('.pbSetColor')
+const inpColor = document.querySelector(".getColor")
+const pbSetColor = document.querySelector(".pbSetColor")
+const pbSetColorNum = document.querySelector(".pbSetColorNum")
 
-const inpColorH1 = document.querySelector('.getColorH1')
-const setColorH1 = document.querySelector('.pbSetColorH1')
-
-
-// Change background Color
 out(inpColor)
 
 const bdy = document.querySelector("body")
 out(bdy)
 
 function changeBodyColor() {
-  let col = inpColor.value
+  let col = inpColor.value;
   out(col)
   bdy.style.backgroundColor = col
 }
 
+function changeBodyColorNum() {
+  let col = inpColor.value;
+  const num = Number(col)
+  const numhx = parseInt(col, 16)
+  out(num)
+  out(numhx)
+  let hexcol = new Number(numhx).toString(16)
+  if (hexcol.length == 2) {hexcol = "0000" + hexcol}
+  if (hexcol.length == 4) {hexcol = "00" + hexcol}
+  out(hexcol)
+  bdy.style.backgroundColor = "#" + hexcol
+}
+
+function changeBodyColorKey(key) {
+  out(key)
+  changeBodyColorNum();
+}
+
 pbSetColor.addEventListener('click', changeBodyColor)
+pbSetColorNum.addEventListener('click', changeBodyColorNum)
+
+inpColor.addEventListener('input', changeBodyColorKey)
 
 ///////////////\\\\\\\\\\\\\\
 
--- js/types.js
+++ js/types.js
@@ -2,21 +2,24 @@ function out(any) {console.log(any)}
 out('I am in types')
 
 function showTypes(lst) {
-  out('START showing Types')
+  out("START Showing Types")
   for (const obj of lst) {
-    out(obj + ' is of the type = ' + typeof obj)
+    out(obj + " is of the type=" + typeof obj)
   }
 }
 
-let arr = [1, 2]
+let arr = [1,2]
 out(arr)
 
-arr = [1, 5, "hello", 5.5, 4>3, [1, 2], {"firstName" : "Erik"}, null]
+arr = [1, 5, "hello", 5.5 , 4>3, [1,2], {"firstName" : "Erik"}, null, 'a']
 out(arr)
 showTypes(arr)
-out('hello there')
+out("hello there")
 
 let aa
 arr.push(aa)
+showTypes(arr)
+let n = 0/0
+arr.push(n)
 showTypes(arr)
 
